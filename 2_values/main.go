bbbe2e8e4be747cf1d6cc0ccffc8f5388b6aed8d
Vansh16aug <kumarvansh16aug@gmail.com>
2024-12-08 17:29:22 +0530

    data types

---
+++ 2_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//int
+	fmt.Println(1);
+	//strings
+	fmt.Println("Vansh");
+	//bool
+	fmt.Println(true);
+	fmt.Println(false);
+	//float
+	fmt.Println(2.3 / 2);
+}
