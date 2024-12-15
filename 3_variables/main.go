9d62715678709153da444cebadca80995ce0ecc8
Vansh16aug <kumarvansh16aug@gmail.com>
2024-12-15 20:51:10 +0530

    variables

---
+++ 3_variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var name string = "Vansh kumar"
+
+	// infering of value
+	// var name = "Vansh kumar"
+
+	name := "Vansh kumar" 
+
+	fmt.Println(name)
+}
