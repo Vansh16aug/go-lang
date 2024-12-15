891c307d13e8e7989276abc0258a319084eb089e
Vansh16aug <kumarvansh16aug@gmail.com>
2024-12-15 21:04:31 +0530

    for loop

---
+++ 5_for/for.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main(){
+	//while loop
+	// i := 1
+	// for i<=3 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	//infinite loop
+	// for {
+	// 	fmt.Println("1")
+	// }
+
+	//basic for loop
+	// for i := 1 ; i <= 10 ; i ++{
+		// if i == 2 {
+		// 	continue;
+		// }
+		// if i == 7{
+		// 	break;
+		// }
+	// 	fmt.Println(i)
+	// }
+
+	// range
+	for i := range 11{
+		fmt.Println(i);
+	}
+}
