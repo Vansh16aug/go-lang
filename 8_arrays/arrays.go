18d2ae82b7bd16f6e90cd9a9e4306c53343de5e1
Vansh16aug <kumarvansh16aug@gmail.com>
2024-12-16 21:58:29 +0530

    arrays

---
+++ 8_arrays/arrays.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var nums[5]int
+	// nums[0]=1
+
+	//predefined array
+	// nums := [5]int {1,2,3,4,5}
+
+	//2d array
+	nums := [2][2] int {{1,2},{3,4}}
+ 
+	fmt.Println(nums)
+
+	// fmt.Println(len(nums))
+	// fmt.Println(nums[0]) 
+	// int = 0
+	// bool = false
+	// string = ""
+}
