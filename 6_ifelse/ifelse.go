f513ec4eac7e22e623ff9f0da699217a563c7e31
Vansh16aug <kumarvansh16aug@gmail.com>
2024-12-16 19:53:39 +0530

    if else

---
+++ 6_ifelse/ifelse.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+    // age := 16
+
+    // if age >=18{
+    //     fmt.Println("person can vote");
+    // }else{
+    //     fmt.Println("person cant vote")
+    // }
+
+    if age := 16; age>=18{
+        fmt.Println("can vote",age)
+    }
+
+}
