ea1b211555fce463c58cba1ff1584068117c638b
Vansh16aug <kumarvansh16aug@gmail.com>
2025-04-06 11:05:17 +0530

    range

---
+++ 11_range/range.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	//array
+	// Iterating over data structures
+	// nums := []int{1, 2, 3, 4, 5}
+	// for i := 0; i < len(nums); i++ {
+	// 	println(nums[i])
+	// }
+
+	// sum := 0
+	// for i, num := range nums {
+	// 	// sum += num
+	// 	fmt.Println(i, num)
+	// }
+	// fmt.Println(sum)
+
+	// map
+	// m := map[string]string{"fname":"john", "lname":"doe"}
+	// for k, v := range m {
+	// 	println(k, v)
+	// }
+
+	//strings (unicode)
+	// for i,c := range "hello" {
+	// 	println(i, string(c))
+	// }
+}
